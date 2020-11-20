e5d9252ca4b9365c5064c7e660b942a87837b040
Ildana <ildana.ruzybayeva@gmail.com>
2020-11-20 09:40:34 +0000

    GET and POST comments to random posts

--- comments/index.js
+++ comments/index.js
@@ -4,14 +4,19 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const { randomBytes } = require("crypto");
 
-const comments = {};
+const commentsByPostId = {};
 
-app.get("/comments", (req, res) => {
-  res.send(comments);
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts", (req, res) => {
-  res.send("hi from comments");
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
+  const comments = commentsByPostId[req.params.id] || []
+  comments.push({id: commentId, content})
+  commentsByPostId[req.params.id] = comments
+  res.status(201).send(comments)
 });
 
 app.listen(4001, () => console.log("listening on 4001"));
