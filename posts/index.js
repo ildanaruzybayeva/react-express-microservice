dc50b2f55e791ef495907471b1ce4621c6efad01
Ildana <ildana.ruzybayeva@gmail.com>
2020-12-01 13:09:15 +0000

    Updated dockerfile for each service

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -13,14 +14,32 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
+
   posts[id] = {
     id,
     title
   };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title
+    }
+  });
+
   res.status(201).send(posts[id]);
 });
 
-app.listen(4000, () => console.log("listening on 4000"));
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
